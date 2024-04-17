376ab16fd0e55ee492a90e649cced3290bb29cb7
NicholasHickmanKeuda <162548631+NicholasHickmanKeuda@users.noreply.github.com>
2024-04-17 11:00:20 +0300

    Navbar & logo
    Lisäsin navbarin ja logon muihin kansioihin myös.

--- Yhteystiedot/navbar.js
+++ Yhteystiedot/navbar.js
@@ -3,7 +3,7 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "225px";
     document.getElementById("main").style.marginLeft = "225px";
   }
-  
+
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
