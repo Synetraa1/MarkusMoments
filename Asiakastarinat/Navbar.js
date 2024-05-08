a3ae930e6c71d6c65c971c14ae964ba03e86c8e3
RukhanaAliyeva96 <156576350+RukhanaAliyeva96@users.noreply.github.com>
2024-05-08 11:03:51 +0300

    Ratings lisäys

---
+++ Asiakastarinat/Navbar.js
@@ -0,0 +1,10 @@
+/* Sivu burgerin leveys */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "225px";
+    document.getElementById("main").style.marginLeft = "225px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
